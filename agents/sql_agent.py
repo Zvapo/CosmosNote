c2d2d9f8d79e8cf298f016e37d4220c1058335be
Kuba Kaczmarek <jakub.kaczmarek@tpa-group.pl>
2025-02-08 01:05:15 +0100

    Add sql agent

--- agents/sql_agent.py
+++ agents/sql_agent.py
@@ -1,1 +1,32 @@
-# agent to write sql queries and query the supabase database
+import os
+from langchain_community.utilities import SQLDatabase
+from langchain_community.agent_toolkits import create_sql_agent
+from langchain.agents import AgentType
+from langchain.chat_models import ChatOpenAI
+from langgraph.types import Command
+from typing import Literal
+from dotenv import load_dotenv
+
+from state import GraphState
+
+load_dotenv()
+
+
+async def topic_agent(state: GraphState) -> Command[Literal["topic_agent"]]: 
+
+    DB_URI = os.getenv("DB_URI")
+    # Change from uri to connection string
+    db = SQLDatabase.from_uri(DB_URI)
+
+    llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0)
+
+    agent_executor = create_sql_agent(
+        llm=llm,
+        db=db,
+        agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+        verbose=True
+    )
+
+    query = "What are the top 5 biggest exoplanets?"
+    response = agent_executor.run(query)
+    print(response)
