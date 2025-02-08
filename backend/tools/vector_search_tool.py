69413ce26ae983d59e08ecc219b858a4abd31b73
Kuba Kaczmarek <jakub.kaczmarek@tpa-group.pl>
2025-02-08 17:29:39 +0100

    Update vector search tool

--- backend/tools/vector_search_tool.py
+++ backend/tools/vector_search_tool.py
@@ -1,18 +1,36 @@
+import os
 from langchain_core.tools import tool
 from langchain_core.embeddings.embeddings import Embeddings
 from langchain_community.vectorstores import SupabaseVectorStore
 from supabase.client import create_client
 from sentence_transformers import SentenceTransformer
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 
 class SentenceEmbeddings(Embeddings):
     def __init__(self):
         self.model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
 
     def embed_documents(self, texts: list[str]) -> list[list[float]]:
-        return [self.encode(text) for text in texts]
+        embeddings = self.model.encode(texts)
+        return embeddings.tolist()
     
     def embed_query(self, text: str) -> list[float]:
-        return self.model.encode(text)
+        embedding = self.model.encode(text)
+        return embedding.tolist()
+
+
+supabase_client = create_client(os.getenv("SUPABASE_URL"), os.getenv("SUPABASE_API_KEy"))
+embeddings = SentenceEmbeddings()
+vector_store = SupabaseVectorStore(
+    client=supabase_client,
+    embedding=embeddings,
+    table_name="document_vectors_test",
+    query_name="match_documents",
+)
 
 @tool
 def vector_search_tool(query: str):
@@ -22,15 +40,10 @@ def vector_search_tool(query: str):
     Args:
         query (str): The search query to execute
     """
-    supabase_client = create_client("my_supabase_url", "my_supabase_key")
-    embeddings = SentenceEmbeddings()
-    vector_store = SupabaseVectorStore(
-        client=supabase_client,
-        embedding=embeddings,
-        table_name="documents",
-        query_name="match_documents",
-    )
-
     results = vector_store.similarity_search(query)
 
     return results
+
+
+if __name__ == "__main__":
+    print(vector_search_tool.invoke({"query": "How does the redder TESS bandpass affect oscillation amplitude compared to Kepler?"}))
