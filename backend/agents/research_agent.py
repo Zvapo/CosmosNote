b1fbc11c93e14c194141b3ce09502f9a924f4e43
Kuba Kaczmarek <jakub.kaczmarek@tpa-group.pl>
2025-02-09 00:04:44 +0100

    Update research agent

--- backend/agents/research_agent.py
+++ backend/agents/research_agent.py
@@ -5,7 +5,7 @@ from tools.vector_search_tool import vector_search_tool
 from tools.sql_agent import sql_tool
 from langchain_core.runnables.config import RunnableConfig
 from langgraph.types import Command
-from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.messages import AIMessage, SystemMessage
 
 async def research_agent(state: GraphState, config: RunnableConfig):
     """
@@ -13,19 +13,30 @@ async def research_agent(state: GraphState, config: RunnableConfig):
     """
     system_prompt = SystemMessage(content="""
                                   You are a helpful assistant that can search the web for information.
-                                  Utilize availabe tools to gather information. 
+                                  Utilize availabe tools to gather information. You can make maximum of 3 searches with the web_search_tool.
                                   If you think that the information gathered is enough to anwser the prompt, reply with 'INFORMATION_GATHERED'
                                   """)
+    
+    tool_calls_count = len([True for x in filter(lambda x: isinstance(x, AIMessage), state["messages"]) if x.tool_calls])
+    
+    if tool_calls_count >= 3:
+        return Command(
+            update={
+                "messages": [AIMessage(content="INFORMATION_GATHERED")]
+            }
+        )
+    
     llm = ChatOpenAI(model_name="gpt-4o-mini", temperature=0)
     # max three searches for the agent to use
-    tools = [web_search_tool, vector_search_tool] # sql_tool
+    tools = [web_search_tool, vector_search_tool, sql_tool]
     llm_w_tools = llm.bind_tools(tools)
 
     # Access state as dictionary
     messages = state["messages"]
     response = await llm_w_tools.ainvoke([system_prompt] + messages)
+
     return Command(
         update={
-            "messages": [response]
+            "messages": [response],
         }
     )
